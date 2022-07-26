65d4a1b9616b30c2597799e3efa56dca4a743f81
hectorfhurtado <hurtado.fernando@gmail.com>
2022-07-26 05:40:25 -0500

    initial commit create nuxt app add pinia dependency add tailwindcss dependency add default layout with provided markup and styling add provided assets

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    css: [ '~/assets/css/main.css' ],
+    vite: {
+        server: {
+            watch: {
+                usePolling: true,
+            },
+        },
+    },
+    buildModules: [
+        '@pinia/nuxt',
+        '@vueuse/nuxt',
+        '@nuxtjs/tailwindcss',
+    ],
+    autoImports: {
+        dirs: [ 'composables/**' ]
+    },
+    app: {
+        head: {
+            link: [
+                { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" }
+            ]
+        }
+    },
+})
