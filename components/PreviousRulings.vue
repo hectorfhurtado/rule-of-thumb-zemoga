05f428857aed3b463af55beb87280fdf6b61a3dc
hectorfhurtado <hurtado.fernando@gmail.com>
2022-07-26 10:41:03 -0500

    add spacing details on mobile list

--- components/PreviousRulings.vue
+++ components/PreviousRulings.vue
@@ -13,17 +13,19 @@ function updateVote( vote, name )
   <div>
     <h3 class="mb-6 text-gray-600 text-[28px] font-light leading-7">Previous Rulings</h3>
 
-    <div class="cards-container flex flex-row w-full gap-3 overflow-x-auto snap-x snap-mandatory touch-pan-x">
-        <Card v-for="(dato, index) in pollStore.data" :key="index" class="w-full max-w-[80vw] shrink-0 snap-center"
-            :name="dato.name"
-            :description="dato.description"
-            :category="dato.category"
-            :picture="dato.picture"
-            :lastUpdated="dato.lastUpdated"
-            :votesPositive="dato.votes.positive"
-            :votesNegative="dato.votes.negative"
-            @vote="updateVote"
-        />
+    <div class="cards-container w-[100vw] -mx-4">
+        <div class="flex flex-row w-full gap-4 overflow-x-auto snap-x snap-mandatory touch-pan-x">
+            <Card v-for="(dato, index) in pollStore.data" :key="index" class="w-full max-w-[80vw] shrink-0 snap-center first:ml-4 last:mr-4"
+                :name="dato.name"
+                :description="dato.description"
+                :category="dato.category"
+                :picture="dato.picture"
+                :lastUpdated="dato.lastUpdated"
+                :votesPositive="dato.votes.positive"
+                :votesNegative="dato.votes.negative"
+                @vote="updateVote"
+            />
+        </div>
     </div>
   </div>
 </template>
